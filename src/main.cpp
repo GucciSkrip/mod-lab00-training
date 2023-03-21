0bc27f9ead167d691e4801e4035101a535300c17
GucciSkrip <114502059+GucciSkrip@users.noreply.github.com>
2023-03-21 15:06:30 +0300

    1.1 main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
 
-int main() {
+using namespace std;
+
+int x, n;
+
+int main() 
+{
+    cout << "Enter X = ";
+    cin >> x;
+    cout << "Enter N = ";
+    cin >> n;
+    cout << "Result = " << power(x, n);
     return 0;
 }
