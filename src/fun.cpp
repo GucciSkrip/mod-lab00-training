10d78ae5e1a3edef0bcd3c6862daf0120088c7c9
GucciSkrip <servolkinc@gmail.com>
2023-03-21 15:26:01 +0300

    1.0

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,6 +1,9 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <math.h>
 
-int64_t power(int64_t x, uint16_t n) {
-    return 0;
+int64_t power(int64_t x, uint16_t n)
+{
+    return pow(x, n);
 }
+
